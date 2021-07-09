80718a414b2b0dd91d2ad95f37a2ab9e5f3e0297
Jimmy Wahlberg <jimmy.wahlberg@gmail.com>
2021-07-09 23:20:18 +0200

    Small fixes

--- footer.php
+++ footer.php
@@ -6,34 +6,28 @@
 <footer class="footer">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xs-12 col-sm-6 col-md-3 text-center">
+            <div class="col-xs-6 col-sm-6 col-md-4 text-center">
                 <?php 
                     if(is_active_sidebar('footer-1')) {
                         dynamic_sidebar('footer-1');
                     }           
                 ?>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 text-center">
+            <div class="col-xs-6 col-sm-6 col-md-4 text-center">
                 <?php 
                     if(is_active_sidebar('footer-2')) {
                         dynamic_sidebar('footer-2');
                     }           
                 ?>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 text-center">
+            <div class="col-xs-12 col-sm-6 col-md-4 text-center">
                 <?php 
                     if(is_active_sidebar('footer-3')) {
                         dynamic_sidebar('footer-3');
                     }           
                 ?>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 text-center">
-                <?php 
-                    if(is_active_sidebar('footer-4')) {
-                        dynamic_sidebar('footer-4');
-                    }           
-                ?>
-            </div>
+            
         </div>
 
         <hr>
--- functions.php
+++ functions.php
@@ -85,16 +85,7 @@ function register_footer_widgets() {
         'after_widget' => '</aside>',
         'before_title' => '<h3 class="widget-title">',
         'after_title' => '</h3>',
-    ) ); 
-    register_sidebar( array(
-        'name' => 'Footer 4',
-        'id' => 'footer-4',
-        'description' => 'Appears in the footer area',
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h3 class="widget-title">',
-        'after_title' => '</h3>',
-    ) );    
+    ) );   
 }
 
 add_action( 'widgets_init', 'register_footer_widgets');
